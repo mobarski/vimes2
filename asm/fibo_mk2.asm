65442eab591d9c78168546dacad58bd2f9870007
Maciej Obarski <mobarski@gmail.com>
2024-01-25 23:06:29 +0100

    test3 -> fibo:

--- asm/fibo_mk2.asm
+++ asm/fibo_mk2.asm
@@ -1,29 +1,23 @@
+; def fib(n)
+;   return n if n <= 1
+;   fib(n - 1) + fib(n - 2)
+; end
 
-; http://dada.perl.it/shootout/fibo.html
+INT 2              ; var 1:n 2:result
+EX1 GETI STO 1 0   ; get n from stdin
 
-LIT 10      ; n = number of fibonacci numbers to calculate
-INT 1       ; allocate space for n
-STO 0 0     ; store n
-
-INT 1       ; allocate space for result
-
-CAL fib 0   ; call main function
-JMP 0       ; halt
+CAL fib 0          ; call fib(n)
+LOD 2 0 EX1 PUTI   ; print result
+JMP 0              ; halt
 
 fib:
-LOD 0 1     ; load n from upper stack frame [0]
-INT 1 STO 0 ; store n in current stack frame [0]
-
-STO 1 1     ; store n in upper stack frame [1]
-OPR RET     ; return
-
-LIT -1      ; XXX just for testing
-LIT -42     ; XXX just for testing
-
-;   	INT 1 STO 0 ; capture n from stack
-;   	LOD 0 LIT 2 OPR LT JZ @else ; n < 2
-;   		LIT 1 OPR RET ; return 1
-;   	else:
-;   		LOD 0 LIT 2 OPR SUB CAL fib ; fib(n-2)
-;   		LOD 0 LIT 1 OPR SUB CAL fib ; fib(n-1)
-;   		OPR ADD OPR RET
+    INT 3
+    INT 1 LOD 1 1 STO 3 0           ; var 3:nn = n
+    LOD 3 0 LIT 1 OPR LE JPC gt1    ; nn <= 1 ?
+        LOD 3 0 STO 2 1 OPR RET     ; result=n
+    gt1:
+        LOD 3 0 LIT 1 OPR SUB  STO 1 1 CAL fib 1  LOD 2 1 ; fib(n-1)
+        LOD 3 0 LIT 2 OPR SUB  STO 1 1 CAL fib 1  LOD 2 1 ; fib(n-2)
+        ; add, store result and return
+        OPR ADD STO 2 1
+        OPR RET                         
