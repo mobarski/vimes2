afbf60af4294345620aa144dd3e2848e3e68f4bc
Maciej Obarski <mobarski@gmail.com>
2024-01-27 16:33:20 +0100

    ex1 fix

--- nim/ex1.nim
+++ nim/ex1.nim
@@ -5,9 +5,9 @@ import vimes/sio
 proc ex1(a:Cell, t:var Cell) =
     case a:
         of PUTI: stdout.write s[t]; t-=1
-        of GETI: t+=1; s[t] = sio.read_int().Cell
         of PUTC: stdout.write chr(s[t]); t-=1
-        of GETC: t+=1; s[t] = stdin.read_char.Cell
+        of GETI: t+=1; s[t] = sio.read_int().Cell
+        of GETC: t+=1; s[t] = sio.read_chr().Cell
         of EOF:  t+=1; s[t] = if sio.eof: 1 else: 0
         else: quit("unknown EX1 opcode",1)
 
@@ -15,8 +15,8 @@ proc ex1(a:Cell, t:var Cell) =
 proc ex1c(a:Cell, code:openArray[Cell]) =
     case a:
         of PUTI: stdout.write s[t]; t-=1
-        of GETI: t+=1; s[t] = sio.read_int().Cell
         of PUTC: stdout.write chr(s[t]); t-=1
-        of GETC: t+=1; s[t] = stdin.read_char.Cell
+        of GETI: t+=1; s[t] = sio.read_int().Cell
+        of GETC: t+=1; s[t] = sio.read_chr().Cell
         of EOF:  t+=1; s[t] = if sio.eof: 1 else: 0
         else: quit("unknown EX1 opcode",1)
