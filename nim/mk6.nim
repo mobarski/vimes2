f9ef3c8b30dff3ccd3caa6d6813ad13db5a37184
Maciej Obarski <mobarski@gmail.com>
2024-01-28 18:50:31 +0100

    mk6 cli support

--- nim/mk6.nim
+++ nim/mk6.nim
@@ -7,21 +7,31 @@ import bench
 
 include mk6_opcodes
 include mk6_opnames
-
 type Cell = int16
 
 var pc: Cell # program counter
 var sp: Cell # stack pointer
-var mem: seq[Cell] = @[] # memory
-var stack: seq[Cell] = @[] # return stack
 var cc: int64 # used only when -d:cc is passed
-var code: seq[Cell] = @[] # program
+var mem:   seq[Cell] = @[] # memory
+var stack: seq[Cell] = @[] # return stack
+var code:  seq[Cell] = @[] # program
+
+proc reset(quick=false) =
+    pc=0; sp=0; cc=0
+    if quick: return
+    for j in low(mem)..high(mem):
+        mem[j] = 0
+    for j in low(stack)..high(stack):
+        stack[j] = 0
 
 proc trace(op,a,b:Cell) =
     let opname = opnames[cast[Instr](op)]
     let va = mem[a]
     let vb = mem[b]
     stderr.write_line """| {cc:3} | {pc:2} | {sp:2} | {opname:>4} {a:2} {b:2} | {va:3} | {vb:3} | """.fmt
+
+proc debug() =
+    echo "pc:", pc, " sp:", sp, " cc:", cc
 
 proc run() =
     while true:
@@ -72,13 +82,17 @@ proc run() =
                 quit("unknown opcode op:" & $op, 1)
         if pc==0: break
 
+include cli
 if is_main_module:
-    # python3 asm.py ../nim/mk6_opcodes.nim ../asm/loops3_mk6.asm -fb10 -o ../asm/b10/loops3_mk6.txt
-    let input_path = "../asm/b10/loops3_mk6.txt"
-    let text = read_file(input_path)
-    code = code_from_num[Cell](text)
     mem = new_seq[Cell](1024)
-    var b = new_bench()
-    run()
-    b.done(cc)
-    b.show()
+    stack = new_seq[Cell](1024)
+    cli()
+    # python3 asm.py ../nim/mk6_opcodes.nim ../asm/loops3_mk6.asm -fb10 -o ../asm/b10/loops3_mk6.txt
+    # let input_path = "../asm/b10/loops3_mk6.txt"
+    # let text = read_file(input_path)
+    # code = code_from_num[Cell](text)
+    # mem = new_seq[Cell](1024)
+    # var b = new_bench()
+    # run()
+    # b.done(cc)
+    # b.show()
