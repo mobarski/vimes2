6c2a210e01f1f9a59bc4dbe35060d263d0f8cd00
Maciej Obarski <mobarski@gmail.com>
2024-01-28 18:52:25 +0100

    example fix

--- asm/miki-add-two-numbers-mk6.asm
+++ asm/miki-add-two-numbers-mk6.asm
@@ -1,5 +1,5 @@
 GETI 0 0 ; pobierz liczbę i zapisz w komórce 0
 GETI 1 0 ; pobierz liczbę i zapisz w komórce 1
-ADD 0 1  ; dodaj komórki 0 i 1 i zapisz w komórce 0
+ADD  0 1 ; dodaj komórki 0 i 1 i zapisz w komórce 0
 PUTI 0 0 ; wypisz komórkę 0
-JMP 0 0 ; zakończ program
+JMP  0 0 ; zakończ program
