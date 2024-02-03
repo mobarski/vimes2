82e49886dbab2e9be22cab3f3e7dd6e682a06ed1
Maciej Obarski <mobarski@gmail.com>
2024-02-04 00:33:35 +0100

    fibo mk8 kv2

--- asm/fibo_mk8_kv.asm
+++ asm/fibo_mk8_kv.asm
@@ -3,49 +3,44 @@
 #   fib(n - 1) + fib(n - 2)
 # end
 
-# mem 0 -> n
-# mem 1 -> a=1
-# mem 2 -> b=2
-# mem 3 -> tos (top-of-stack) = 10
-# mem 4 -> tmp
-
+# mem
 n:0
 one:1
 two:2
-tos:3
+sp:3   # stack pointer
 tmp:4
 
-lit 10 sta tos # m[3] = 10
+lit 10 sta sp # stack starts at m[10]
 
 in 0 sta n
-inc tos spa tos # push
+inc sp spa sp # push
 lit 1 sta one
 lit 2 sta two
 cal fib
-lpa tos out 0 # pop print
+lpa sp out 0 # pop print
 hlt 0
 
 fib: (n--y)
-    lpa tos
+    lpa sp
     sub one
     jz retn
-    lpa tos
+    lpa sp
     sub one
     jn retn
 
-    lpa tos sub one # n-1
-    inc tos spa tos # (n n1)
+    lpa sp sub one # n-1
+    inc sp spa sp # (n n1)
     cal fib (n y1)
 
-    lda tos sta tmp dec tmp # tmp:tos-1
+    lda sp sta tmp dec tmp # tmp:sp-1
     lpa tmp sub two   # n-2
-    inc tos spa tos # (n y1 n2)
+    inc sp spa sp # (n y1 n2)
     cal fib (n y1 y2)
 
-    lpa tos dec tos (n y1)
+    lpa sp dec sp (n y1)
     sta tmp # tmp:y2
-    lpa tos dec tos (n)
-    add tmp spa tos (y1+y2)
+    lpa sp dec sp (n)
+    add tmp spa sp (y1+y2)
 
     ret 0
 
---
+++ asm/fibo_mk8_kv2.asm
@@ -0,0 +1,55 @@
+# def fib(n)
+#   return n if n <= 1
+#   fib(n - 1) + fib(n - 2)
+# end
+
+# commands
+store:sta
+load:lda
+poke:spa
+peek:lpa
+
+# mem
+n:0
+one:1
+two:2
+sp:3   # stack pointer
+tmp:4
+
+lit 10 store sp # stack starts at m[10]
+
+in 0 store n
+inc sp poke sp # push
+lit 1 store one
+lit 2 store two
+cal fib
+peek sp out 0 # pop print
+hlt 0
+
+fib: (n--y)
+    peek sp
+    sub one
+    jz retn
+    peek sp
+    sub one
+    jn retn
+
+    peek sp sub one # n-1
+    inc sp poke sp # (n n1)
+    cal fib (n y1)
+
+    load sp store tmp dec tmp # tmp:sp-1
+    peek tmp sub two   # n-2
+    inc sp poke sp # (n y1 n2)
+    cal fib (n y1 y2)
+
+    peek sp dec sp (n y1)
+    store tmp # tmp:y2
+    peek sp dec sp (n)
+    add tmp poke sp (y1+y2)
+
+    ret 0
+
+    retn:
+        (n--n)
+        ret 0
