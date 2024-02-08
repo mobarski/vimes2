8093320598ab3233d7de9cc69de98505c755ec0e
Maciej Obarski <mobarski@gmail.com>
2024-02-08 22:27:15 +0100

    mk7ci

---
+++ c/mk7ci.c
@@ -0,0 +1,53 @@
+#include "mk7ci.h"
+#include "cli.h"
+
+void reset(int quick) {
+    if (!quick) reset_mem();   // from cli.h
+    if (!quick) reset_stack(); // from cli.h
+}
+
+// TODO trace
+// TODO debug
+
+void run() {
+
+    register Word pc = 0;    // program counter
+    register Word acc = 0;   // accumulator
+    register int64_t _cc = 0; // cycle counter
+    Word tmp; // used in _IN, as acc is assigned to a register
+    Word n;
+
+    // cc++ in BEFORE is faster than cc++ in NEXT !!!
+    #define BEFORE _cc++
+    #define op     code[pc]
+    #define a      code[pc+1]
+    #define NEXT   goto *labels[op]
+
+    void *labels[] = {
+        &&_HLT, &&_IN, &&_OUT, &&_LDA, &&_STA, &&_ADD, &&_SUB, &&_INC, &&_DEC, &&_JMP, &&_JZ, &&_JN, &&_LIT
+    };
+
+    NEXT;
+    _JZ:  BEFORE; pc = (acc==0)? a :          pc+2;  NEXT;
+    _JN:  BEFORE; pc = (acc<0) ? a :          pc+2;  NEXT;
+    _JMP: BEFORE;                             pc=a;  NEXT;
+    _LDA: BEFORE; acc=mem[a];                 pc+=2; NEXT;
+    _LIT: BEFORE; acc=a;                      pc+=2; NEXT;
+    _STA: BEFORE; mem[a]=acc;                 pc+=2; NEXT;
+    _ADD: BEFORE; acc+=mem[a];                pc+=2; NEXT;
+    _SUB: BEFORE; acc-=mem[a];                pc+=2; NEXT;
+    _INC: BEFORE; mem[a]++;                   pc+=2; NEXT;
+    _DEC: BEFORE; mem[a]--;                   pc+=2; NEXT;
+    _OUT: BEFORE; printf("%d ", acc);         pc+=2; NEXT;
+    _IN:  BEFORE; n=fscanf(stdin, "%hd", &tmp);
+                  /* TODO: handle n==0 */
+                  acc=tmp;                    pc+=2; NEXT;
+    _HLT: BEFORE;                             goto   END;
+
+    END:
+    cc = _cc;
+}
+
+int main(int argc, char *argv[]) {
+    return cli_main(argc, argv);
+}
---
+++ c/mk7ci.h
@@ -0,0 +1,33 @@
+#include <stdint.h>
+
+typedef enum {
+	HLT = 0,
+    IN  = 1,
+    OUT = 2,
+    LDA = 3,
+    STA = 4,
+    ADD = 5,
+    SUB = 6,
+    INC = 7,
+    DEC = 8,
+    JMP = 9,
+    JZ  = 10,
+    JN  = 11,
+    // EXTENSION
+    LIT = 12,
+} Instr;
+
+typedef int16_t Word;
+
+Word pc = 0; // program counter (not used)
+Word acc = 0; // accumulator (not used)
+Word *mem; // memory
+Word *code; // code
+Word *stack; // stack (not used)
+Word mem_size; // memory size
+Word code_size; // code size
+Word stack_size; // stack size
+int64_t cc = 0; // cycle counter
+
+void reset(int quick);
+void run();
