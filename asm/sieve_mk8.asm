8178a2f4177cb4233c2249246ea3b02c6d4f5953
Maciej Obarski <mobarski@gmail.com>
2024-02-09 18:33:50 +0100

    sieve mk8

---
+++ asm/sieve_mk8.asm
@@ -0,0 +1,48 @@
+
+into=sta
+load=lda
+peek=lpa
+poke=spa
+
+n=2
+i=3 p=4
+j=5 q=6
+array=10
+
+lit 0 into 0
+lit 1 into 1
+
+in 0 into n
+load n into i
+lit array into p
+
+loop:
+    load i jz loop_end
+    lit 1 poke p
+    dec i
+    inc p
+    jmp loop loop_end:
+
+#jmp end # TEST point #1 all numbers set to 1
+
+lit 2 into i
+lit array add i into p
+loop2:
+    load n sub i jn end
+    peek p jz not_prime
+        load i out 0
+        # TODO: dont zero this number !!!
+    not_prime:
+
+    load p into q
+    load i into j
+    loop3:
+        load n sub j jn loop3_end
+        lit array add j into q
+        lit 0 poke q
+        load j add i into j
+        jmp loop3 loop3_end:
+    inc p inc i
+    jmp loop2
+
+end:
