5d46395a5909726a04a8f500464300bf4197c828
Maciej Obarski <mobarski@gmail.com>
2024-02-19 00:05:27 +0100

    isort mk10 WIP

--- asm/isort_mk10.asm
+++ asm/isort_mk10.asm
@@ -16,22 +16,49 @@ n=5
 i=6
 j=7
 a=8
-val=9
+ai=9
+aj=10
+val=11
+acc=12
+aptr=19
 array=20
 
 # aliasses:
 call=cal
-
+cmp=sub
+jeq=jz
+jlt=jn
+jgt=jp
 
 main:
     lit 0 0
     lit 1 1
+    lit 2 2
+    lit aptr array
     call load-data 0
-    #call sort 0
+    call sort 0
     call show 0
     hlt 0 0
 
 sort:
+    lit i 1
+    lit ai array
+    add ai 1
+    .loop1: mov acc i cmp acc n add acc 1 jeq .end1 acc
+        ptm val ai
+        mov aj ai
+        sub aj 1
+        .loop2: mov acc aj cmp acc aptr jlt .end2 acc
+                     (and) ptm acc aj cmp acc val jlt .end2 acc jeq .end2 acc
+            ptm acc aj
+            add aj 1
+            mtp aj acc
+            sub aj 2
+            jmp .loop2 0 .end2:
+        add aj 1
+        mtp aj val
+        sub aj 1
+        add i 1 add ai 1 jmp .loop1 0 .end1:
     ret 0 0
 
 load-data:
